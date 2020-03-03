63d1a16cdb9d2ae6b5e52a445ac15a4e0cad0a0a
Timothy Kroon <timothykroon1@gmail.com>
2020-03-03 13:29:36 +0100

    Uitwerking van de exercise

---
+++ multiplier.php
@@ -0,0 +1,14 @@
+<?php
+    echo "ik ga twee getallen met elkaar vermenigvuldigen";
+
+    echo "Eerste getal?" . PHP_EOL;
+    $eersteGetal = readline();
+
+    echo "Tweede Getal?" . PHP_EOL;
+    $tweedeGetal = readline();
+
+    $uitkomst = $eersteGetal * $tweedeGetal;
+
+    echo "Uw resultaat is:". $uitkomst . PHP_EOL;
+
+    ?>
